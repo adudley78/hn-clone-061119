d3eeb9c0e8fa1a7faabdde25aa4ee4423c0674d0
adudley78 <dudleyadam@gmail.com>
2019-06-11 13:36:28 -0400

    Create folders, organize files, update App/index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+import GlobalStyles from './styles/globals';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderApp = () => {
+    ReactDOM.render(
+        <div>
+            <GlobalStyles />
+            <App />
+        </div>
+        ,document.getElementById('root')
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+renderApp();
+
 serviceWorker.unregister();
